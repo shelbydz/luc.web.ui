a41f1f963be3cde9833ea04c66fe70741c1797b9
shawn <shelbydz@gmail.com>
2015-07-18 13:21:55 -0400

    update code added

--- app/controllers/luc.controller.js
+++ app/controllers/luc.controller.js
@@ -25,7 +25,7 @@
             self.submit = function () {
                 //TODO: rest call to submit data
                 console.log('submitted ' + self.model.name + ' ' + self.model.airDate);
-                self.model.airDate = filter('date')(self.model.airDate, 'yyyy-MM-dd')
+                self.model.airDate = filter('date')(self.model.airDate, 'yyyy-MM-dd');
                 var data = angular.toJson(self.model);
                 http.post('http://localhost:5000/episode', data)
                     .success(function(){
@@ -55,18 +55,23 @@
         }
     ]);
 
-    lucController.controller('resultsController', ['$http', '$scope',
-        function (http, scope ) {
+    lucController.controller('resultsController', ['$http', '$scope', '$filter',
+        function (http, scope, filter) {
             var self = this;
+            self.editable = '<input ng-class=';
             self.model = [];
             self.message = 'results';
+
             self.gridOptions = {
                 data: 'vm.model',
+                enableCellSelection: true,
+                enableRowSelection: false,
+                enableCellEdit: true,
                 columnDefs:[
                     {field:'episodeNumber', displayName:'Number'},
                     {field:'writer', displayName:'Writer'},
                     {field:'name', displayName:'Name'},
-                    {field:'airDate', displayName:'Air Date'},
+                    {field:'airDate', displayName:'Air Date', cellFilter:'date'}
                 ]
             };
             self.getData = function() {
@@ -75,13 +80,30 @@
                         self.model = data;
                     })
                     .error(function(data, status, headers, config){
-                        console.error('uh oh!')
+                        console.error('uh oh!');
+                        console.error(data)
                     })
-            }
+            };
             self.getData();
 
+            var updateRow = function(data) {
 
-            //self.getData();
+                data.airDate = filter('date')(data.airDate, 'yyyy-MM-dd');
+
+                http.post('http://localhost:5000/episode', data)
+                    .success(function(){
+                        console.log(('great success!'));
+                    })
+                    .error(function(){
+                       console.error('uh oh');
+                    });
+            };
+
+            scope.$on('ngGridEventEndCellEdit', function(evt){
+                console.log(evt.targetScope.row.entity);
+                updateRow(evt.targetScope.row.entity);
+            })
+
         }
     ])
 
